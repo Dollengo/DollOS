4d5ea9b96c42555a6c2b73b687bf15144d45f626
Tarcísio <131563888+Dollengo@users.noreply.github.com>
2024-06-19 10:12:11 -0300

    Add files via upload

---
+++ src/system/main/code/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if ! command -v python &> /dev/null; then
+    echo "Python not found. Intalling Python..."
+    sudo apt update
+    sudo apt install python3
+else
+    echo "Python installed."
+fi
+
+echo "Installing python dependencies..."
+sudo pip install datetime psutil rich pandas
+
+echo "Starting..."
+echo "Package: DollOS
+Version: 1.2
+Section: base
+Priority: optional
+Architecture: all
+Depends: python3, python3-pip, python3-datetime, python3-psutil, python3-rich, python3-pandas
+Maintainer: Tarcisio <dollengo@outlook.com>
+Description: Library for Linux beginners. GitHub: github.com/dollengo/dolloslib
+"
+python DollOS.py
